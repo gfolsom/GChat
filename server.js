f6b9ce810b479f6c8b05663bc608e8521dc34fb1
Grant Folsom <grant.folsom@gmail.com>
2018-09-19 17:07:28 -0500

    Updated Functionality

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/start', (req, res) => {
+app.get('/#', (req, res) => {
   res.sendFile(path.join(__dirname +'/index.html'));
 });
 
